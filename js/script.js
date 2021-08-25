8212ec6b7609dba0122ddca1521614d76151c8ff
Wilkenson Hilarion <wilkenson@Wilkensons-MBP.home>
2021-08-25 16:40:29 -0400

    added functionality to calc

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-00;
-console.log("00:", 00);
 
-let runningTotal = 0;
-let buffer = "0";
-let prevOperator;
-const screen = document.querySelector(".screen");
+
+let runningTotal = 0; // keep track of running number
+let buffer = "0"; //resetting
+let prevOperator = null; // keep track of  previous operator
+const screen = document.querySelector(".screen"); // inherent by reRender ƒ();
 
 document
   .querySelector(".btn-container")
@@ -27,7 +26,6 @@ function handleNum(value) {
   } else {
     buffer += value;
   }
-  
 }
 
 function handleSymbol(value) {
@@ -36,7 +34,7 @@ function handleSymbol(value) {
       buffer = "0";
       runningTotal = null;
       return;
-    case "=":
+    case "=":       
       if (prevOperator === null) {
         return;
       }
@@ -52,11 +50,34 @@ function handleSymbol(value) {
         buffer = buffer.substring(0, buffer.length - 1);
       }
       return;
-      default:handleMath(value);
+    default:
+      handleMath(value);
       break;
   }
 }
+function handleMath(value) {
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
 
+  prevOperator= value;
+
+  buffer = "0";
+}
+function flushOperation(intBuffer) {
+  if (prevOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (prevOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (prevOperator === "x") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
 function reRender(value) {
   screen.innerText = buffer;
 }
